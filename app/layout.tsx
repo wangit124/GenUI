f94bfa4bbd9cf280f166585af36894f35a90b923
wangit124 <luhao.wang2016@gmail.com>
2025-07-19 20:32:44 -0700

    [monitor] speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
+        <SpeedInsights />
         <Providers>{children}</Providers>
       </body>
     </html>
