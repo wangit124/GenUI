b7caf1dfb4020f6fb46a4db71a02dee07240500f
wangit124 <luhao.wang2016@gmail.com>
2025-07-30 09:34:22 -0700

    return success

--- app/api/ping_db/route.ts
+++ app/api/ping_db/route.ts
@@ -2,11 +2,11 @@ import { supabase } from "@/lib/supabase";
 
 export async function GET() {
   try {
-    const { data, error } = await supabase.from("users").select().limit(1);
+    const { error } = await supabase.from("users").select().limit(1);
     if (error) throw new Error(error.message);
-    return Response.json(data);
+    return Response.json({ success: true });
   } catch (error) {
     const message = (error as Error).message ?? "An error occurred.";
-    return Response.json({ error: message }, { status: 400 });
+    return Response.json({ success: false, error: message }, { status: 400 });
   }
 }
